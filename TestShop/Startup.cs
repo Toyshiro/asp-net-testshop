cf371d142e1aecc27263e235c367c89f4bc89146
toyshiro <toyshiro.work@yandex.ru>
2020-02-18 20:04:51 +0500

    in wwwroot add index.html

--- TestShop/Startup.cs
+++ TestShop/Startup.cs
@@ -11,15 +11,17 @@ namespace TestShop
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
