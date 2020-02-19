4aeb6b0477a0784274123a59b58cd3ffefec5f6c
toyshiro <toyshiro.work@yandex.ru>
2020-02-19 20:46:46 +0500

    add models Order and Product in project

---
+++ TestShop/Models/Order.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestShop.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string User { get; set; }
+        public string Adress { get; set; }
+        public string ContactPhone{ get; set; }
+
+        public int ProductId { get; set; }
+        public Product Product{ get; set; }
+    }
+}
