04a988c2e3a23ce12305378391171f160621dbba
toyshiro <toyshiro.work@yandex.ru>
2020-02-19 20:58:03 +0500

    add dbcontext

---
+++ TestShop/Models/ShopContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestShop.Models
+{
+    public class ShopContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+
+        }
+
+
+
+
+    }
+
+}
